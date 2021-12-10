05ed99cca8d6cd00cb6acf06b8c5d3af1d3fe107
Jan <Malion@gmx.net>
2021-12-10 22:07:54 +0100

    Shortened day 10

--- Day_10.py
+++ Day_10.py
@@ -11,50 +11,33 @@ for line in file.readlines():
 #region: Part 1
 values = {')': 3, ']': 57, '}': 1197, '>': 25137}
 pairs = {'(': ')', '[': ']', '{': '}', '<': '>'}
-error_score = 0
-for ind in range(len(subsys_lines)-1, -1, -1):
-    line = subsys_lines[ind]
-    open_chunks = ''
-    for char in line:
-        if char in pairs:                       # opening new parenthesis
-            open_chunks += char
-        elif char == pairs[open_chunks[-1]]:    # closing last parenthesis
-            open_chunks = open_chunks[:-1]
+error_score, counter1 = [0, 0]
+while counter1 < len(subsys_lines):
+    flag, counter2 = [0, 0]
+    while counter2 < len(subsys_lines[counter1]):
+        if subsys_lines[counter1][counter2] in pairs:                       # opening new parenthesis
+            counter2 += 1
+        elif subsys_lines[counter1][counter2] == pairs[subsys_lines[counter1][counter2-1]]:    # closing last parenthesis
+            subsys_lines[counter1] = subsys_lines[counter1][0:counter2-1] + subsys_lines[counter1][counter2+1::]    # Preparation for part 2
+            counter2 -= 1
         else:                                   # erroneus line
-            error_score += values[char]
-            del subsys_lines[ind]               # Preparation for part 2
+            error_score += values[subsys_lines[counter1][counter2]]
+            del subsys_lines[counter1]               # Preparation for part 2
+            flag = 1
             break
-
+    if flag == 0:
+        counter1 += 1
+        flag = 0
 print('Error score for part 1: ' + str(error_score))
 #endregion: Part 1 complete!
 #region: Part 2
-values = {'0': 1, '1': 2, '2': 3, '3': 4}
+values = {'(': 1, '[': 2, '{': 3, '<': 4}
 scores = []
 for line in subsys_lines:
-    open_chunks = ''
-    for char in line:
-        if char == '(':
-            open_chunks += '0'
-        elif char == ')' and open_chunks[-1] == '0':
-            open_chunks = open_chunks[:-1]
-        elif char == '[':
-            open_chunks += '1'
-        elif char == ']' and open_chunks[-1] == '1':
-            open_chunks = open_chunks[:-1]
-        elif char == '{':
-            open_chunks += '2'
-        elif char == '}' and open_chunks[-1] == '2':
-            open_chunks = open_chunks[:-1]
-        elif char == '<':
-            open_chunks += '3'
-        elif char == '>' and open_chunks[-1] == '3':
-            open_chunks = open_chunks[:-1]
     score = 0
-    for char in reversed(open_chunks):
+    for char in reversed(line):
         score = score * 5 + values[char]
     scores.append(score)
 scores.sort()
 print('Error score for part 2: ' + str(scores[int(np.floor(len(scores)/2))]))
-
-
 #endregion: Part 2 complete!
